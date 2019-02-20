70abf81c8727d28b095ed2a3559689ea05983fe5
Dan Burton <danburton.email@gmail.com>
2019-02-19 19:31:32 -0500

    Tweaks to preserve the record backcompat

--- src/Yesod/GitRev/Data.hs
+++ src/Yesod/GitRev/Data.hs
@@ -5,12 +5,14 @@
 
 module Yesod.GitRev.Data where
 
-import GitHash
-import Language.Haskell.TH.Syntax (Lift(lift), Q, TExp, Exp, unsafeTExpCoerce, unTypeQ)
-import Yesod.Core hiding (lift)
+import GitHash (tGitInfoCwd, giHash, giBranch, giDirty, GitInfo)
+import Language.Haskell.TH.Syntax (Lift, Q, TExp, Exp, unTypeQ)
+import Yesod.Core
 
-newtype GitRev = GitRev
-  { gitRevInfo :: GitInfo
+data GitRev = GitRev
+  { gitRevHash :: String
+  , gitRevBranch :: String
+  , gitRevDirty :: Bool
   }
   deriving Lift
 
@@ -19,12 +21,17 @@ mkYesodSubData "GitRev" [parseRoutes|
 |]
 
 tGitRev :: Q (TExp GitRev)
-tGitRev = unsafeTExpCoerce gitRev
+tGitRev = [|| gitRevFromGitInfo $$(tGitInfoCwd) ||]
 
 gitRev :: Q Exp
-gitRev =
-  [|
-    GitRev
-      { gitRevInfo = $(unTypeQ tGitInfoCwd)
-      }
-  |]
+gitRev = unTypeQ tGitRev
+
+-- | This function is considered to be "internal".
+-- Use it at your own peril.
+-- Changes to this function will not be accounted for in minor version bumps.
+gitRevFromGitInfo :: GitInfo -> GitRev
+gitRevFromGitInfo gi = GitRev
+  { gitRevHash = giHash gi
+  , gitRevBranch = giBranch gi
+  , gitRevDirty = giDirty gi
+  }
